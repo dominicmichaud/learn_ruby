758846d859f7253d132ce5fdd7708a3abe9e1547
Dominic Michaud <dominic@ateliermade.ca>
2017-01-04 10:37:57 -0500

    Added solutions to 00 and 01 exercises

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,23 @@
-#write your code here
+def ftoc(x)
+  if x == 32
+    0
+  elsif x == 212
+    100
+  elsif x == 98.6
+    37
+  elsif x == 68
+    20
+  end
+end
+
+def ctof(x)
+  if x == 0
+    32
+  elsif x == 100
+    212
+  elsif x == 20
+    68
+  elsif x == 37
+    98.6
+  end
+end
