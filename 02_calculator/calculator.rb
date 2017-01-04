c4d537b39bdda4399259e41fc79f6951007d8f8c
Dominic Michaud <dominic@ateliermade.ca>
2017-01-04 14:14:19 -0500

    Added solutions to 2 exercises

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,33 @@
-#write your code here
+def add(x,y)
+  x+y
+end
+
+def subtract(x,y)
+  x-y
+end
+
+def sum(x)
+  x.inject(0){|sum,i| sum+i}
+end
+
+def multiply(x)
+  x.inject(:*)
+end
+
+
+def power(x,y)
+  x**y
+end
+
+def factorial(x)
+  if x < 2
+    1
+  else
+    z = []
+    1.upto(x) do|i|
+      z.push(i)
+    end
+
+    z.inject(:*)
+  end
+end
