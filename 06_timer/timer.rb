b0e782dfe28b0a732d9590ad9e6b1b33eaf82d94
Dominic Michaud <dominic@ateliermade.ca>
2017-01-06 09:54:27 -0500

    Added solutions to 06 exercises

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,12 @@
 class Timer
-  #write your code here
+
+  attr_accessor :seconds, :time_string
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    strTime = Time.at(@seconds).utc.strftime("%H:%M:%S")
+  end
 end
